f41e35446d380298ea7c4a4f98f048f276741580
churui <churui@clxkj.cn>
2024-12-28 16:42:20 +0800

    init

---
+++ craco.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const CracoLessPlugin = require("craco-less");
+
+const resolve = (pathName) => path.resolve(__dirname, pathName);
+
+module.exports = {
+  // webpack
+  webpack: {
+    // 配置别名
+    alias: {
+      "@": resolve("src"),
+    },
+  },
+
+  // less
+  plugins: [{ plugin: CracoLessPlugin }],
+};
